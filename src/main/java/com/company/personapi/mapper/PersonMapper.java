a4a29bd17924c352e9290d05f47661cb70175f94
felip <felipe.psic@outlook.com.br>
2021-07-25 11:54:03 -0300

    adição de classes e packtes baixados da aula

---
+++ src/main/java/com/company/personapi/mapper/PersonMapper.java
@@ -0,0 +1,18 @@
+package com.company.personapi.mapper;
+
+import com.company.personapi.dto.request.PersonDTO;
+import com.company.personapi.entity.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
+}
