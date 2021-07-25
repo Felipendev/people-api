6a9f9b21b999918957772573e36bd14b424b01a7
felip <felipe.psic@outlook.com.br>
2021-07-25 18:04:45 -0300

    Added more structure files

--- src/main/java/com/company/personapi/controller/PersonController.java
+++ src/main/java/com/company/personapi/controller/PersonController.java
@@ -1,15 +1,35 @@
 package com.company.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.company.personapi.dto.request.PersonDTO;
+import com.company.personapi.dto.response.MessageResponseDTO;
+import com.company.personapi.entity.Person;
+import com.company.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/people")
 public class PersonController {
 
-    @GetMapping
-    public String getBook() {
-        return "API test";
+   private PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody PersonDTO personDto) {
+        return personService.createPerson(personDto);
+    }
+
+//    @GetMapping
+//    @ResponseStatus()
+//    public List<PersonDTO> listAll(@RequestBody PersonDTO personDto) {
+//        return personService.listAll(personDto.getFirstName());
+//    }
 }
